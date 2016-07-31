dc2300b1c307b90f9925a60c9e8159454b105759
unknown <>
2016-08-01 00:43:11 +0800

    image component construction 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,33 +10,48 @@ let yeomanImage = require('../images/yeoman.png');
 
 // 利用自执行函数，将图片名转成图片URL路径信息
 imageDatas = (function genImageURL(imageDataArr){
-   for (var i = 0,  j = imageDataArr.length; i < j; i++){
-     var sinleImageData = imageDataArr[i];
+   for (let i = 0,  j = imageDataArr.length; i < j; i++){
+     let sinleImageData = imageDataArr[i];
      sinleImageData.imageURL = require('../images/' + sinleImageData.fileName);
      imageDataArr[i] = sinleImageData;
    }
    return imageDataArr;
  })(imageDatas)
-console.log(imageDatas)
-//imageDatas = genImageURL(imageDatas)
 
+class ImgFigure extends React.Component{
+  render() {
+    return(
+      <figure className="img-figure">
+        <img src={this.props.data.imageURL}
+             alt={this.props.data.title} />
+        <figcaption>
+          <h2 className="img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    )
+  }
+}
 
 class GalleryByReactAPP extends React.Component {
   render() {
+    let contorllerUnits = [],
+        imgFigures      = [];
+        imageDatas.forEach(function(value){
+          imgFigures.push(<ImgFigure data={value} />)
+        });
     return (
       <section className="stage">
         <section class="img-sec">
-
+          {imgFigures}
         </section>
         <nav className="controller-nav">
-
+          {contorllerUnits}
         </nav>
       </section>
     );
   }
 }
 
-GalleryByReactAPP.defaultProps = {
-};
+GalleryByReactAPP.defaultProps = {};
 
 export default GalleryByReactAPP;
